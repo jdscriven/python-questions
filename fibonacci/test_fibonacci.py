046f4e79b4b2d73b07a0ddb89f60a9e6b8cf4c13
James Scriven <jdscriven gmail>
2015-11-28 11:22:03 -0500

    More tests, general cleanup

--- fibonacci/test_fibonacci.py
+++ fibonacci/test_fibonacci.py
@@ -4,7 +4,8 @@ from fibonacci import fibonacci
 
 class FibonacciTest(unittest.TestCase):
     def test_first_few(self):
-        """Test the first few elements of the fibonacci sequence, using hard coded values."""
+        """Test the first few elements of the fibonacci sequence,
+           using hard coded values."""
         fibs = fibonacci()
 
         self.assertEqual(0, next(fibs))
@@ -18,7 +19,7 @@ class FibonacciTest(unittest.TestCase):
     def test_1000_elements(self):
         """Test elements 2-1002, by comparing them with the previous 2."""
         fibs = fibonacci()
- 
+
         a = next(fibs)
         b = next(fibs)
         for _ in range(1000):
@@ -30,15 +31,15 @@ class FibonacciTest(unittest.TestCase):
         """Request a sequence with sys.maxsize limit number of elements."""
         fibs = fibonacci(sys.maxsize)
         self.assertEqual(0, next(fibs))
-  
+
     def test_exceed_maxsize_limit(self):
         """Request a sequence with sys.maxsize +1 limit number of elements."""
         self.assertRaises(ValueError, fibonacci, sys.maxsize+1)
-  
+
     def test_negative_limit(self):
         """Request a sequence with a negative limit number of elements."""
         self.assertRaises(ValueError, fibonacci, -1)
-  
+
     def test_zero_limit(self):
         """Request a sequence with a zero limit number of elements."""
         # getting empty generator is fine
@@ -47,9 +48,19 @@ class FibonacciTest(unittest.TestCase):
         self.assertRaises(StopIteration, next, fibs)
 
     def test_one_limit(self):
-        """Request a sequence with a limit of one element. Ensure the generator
-           is exhausted after reading that element."""
+        """Request a sequence with a limit of one element.
+           Ensure the generator is exhausted after reading that element."""
         fibs = fibonacci(1)
+        # first next() call ok
+        next(fibs)
+        # second next() call should fail
+        self.assertRaises(StopIteration, next, fibs)
+
+    def test_one_limit_keyword(self):
+        """Request a sequence with a limit of one element
+           (using a keyword argument).
+           Ensure the generator is exhausted after reading that element."""
+        fibs = fibonacci(limit=1)
         # first next() call ok
         next(fibs)
         # second next() call should fail
---
+++ setup.py
@@ -0,0 +1,7 @@
+from distutils.core import setup
+
+setup(name='fibonacci',
+      version='1.0',
+      py_modules=['fibonacci.fibonacci'],
+      author='James Scriven',
+     )
